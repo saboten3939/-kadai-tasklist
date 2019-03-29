911cd7ce5b4f49dfd1f74ffd373d2724413008b0
taiga.takahashi <saboten11031109@gmail.com>
2019-03-29 10:55:55 +0000

    privateの追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,8 +46,10 @@ class TasksController < ApplicationController
     flash[:success] = 'タスクは正常に削除されました'
     redirect_to tasks_url
   end
-end
+  
+  private
 
   def task_params
     params.require(:task).permit(:content)
   end
+end
